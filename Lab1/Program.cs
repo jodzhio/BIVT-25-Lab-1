65870df0e4cb465fd63572c06b58e36d9c66b37c
Mark <112826355+NemirovichMark@users.noreply.github.com>
2025-09-04 10:26:45 +0300

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,7 +5,7 @@
         public static void Main()
         {
             White white = new White();
-            Console.WriteLine(white.Task1(1.2));
+            //Console.WriteLine(white.Task1(1.2));
             //Console.WriteLine(white.Task2(2));
             //Console.WriteLine(white.Task3(2.5, 1.89));
             //Console.WriteLine(white.Task4(-2.5, 1.89));
@@ -14,5 +14,6 @@
         }
     }
 }
+
 
 
